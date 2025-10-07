b4b7ea7e9f4de4be82e6d872485af932daa8512c
neveler <55753029+neveler@users.noreply.github.com>
2025-10-07 13:37:09 +0800

    更新

--- _build/setup.sh
+++ _build/setup.sh
@@ -5,18 +5,20 @@ exclude_target=("./_build" "./_data" "./_site" "./_site_temp" "./_includes" "./_
 exclude_build_file=("./_site_temp/sitemap.xml", "./_site_temp/robots.txt")
 exclude_build_folder=("./_site_temp/assets")
 
-bundle exec jekyll build --trace --verbose --destination _site --config _config.yml,"$1"
-
 echo "cp -r _data _data_bak"
 cp -r _data _data_bak
+
+bundle exec jekyll build --trace --verbose --destination _site --config _config.yml,"$1"
 
 for config in ./_build/config.*.yml; do
     [ -f "$config" ] || continue
 
-    echo "build $language version"
     language="${config#./_build/config.}"
     language="${language%.yml}"
+    echo "build $language version"
 
+    echo "cp -r _data_bak _data"
+    cp -r _data_bak _data
     for data in ./_data/**/*."$language".*; do
         [ -f "$data" ] || continue
         dest="${data/.$language./.}"
@@ -53,7 +55,4 @@ for config in ./_build/config.*.yml; do
         echo "cp $build_src $build_dst"
         cp "$build_src" "$build_dst"
     done
-
-    echo "cp -r _data_bak _data"
-    cp -r _data_bak _data
 done
