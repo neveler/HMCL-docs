0ea5c255fe263b75e180b01e49c9e7988ce30ffc
neveler <55753029+neveler@users.noreply.github.com>
2025-10-07 12:45:14 +0800

    更新

---
+++ _build/setup.sh
@@ -0,0 +1,51 @@
+shopt -s globstar
+
+exclude_target=("./_build" "./_data" "./_site" "./_site_temp" "./_includes" "./_layouts")
+
+exclude_build_file=("./_site_temp/sitemap.xml", "./_site_temp/robots.txt")
+exclude_build_folder=("./_site_temp/assets")
+
+for config in ./_build/config.*.yml; do
+    [ -f "$config" ] || continue
+
+    echo "build $language version"
+    language="${config#./_build/config.}"
+    language="${language%.yml}"
+
+    for data in ./_data/**/*."$language".*; do
+        [ -f "$data" ] || continue
+        dest="${data/.$language./.}"
+        echo "mv $data $dest"
+        mv "$data" "$dest"
+    done
+
+    for target in ./_*; do
+        [ -f "$target" ] && continue
+        [[ " ${exclude_target[*]} " == *" $target "* ]] && continue
+
+        find $target -type f -name "*.*" ! -name "*.*.*" | while read -r file; do
+            dir="${file%/*}"
+            ext="${file##*.}"
+            base="${file##*/}"
+            name="${base%%.*}"
+            language_file="$dir/$name.$language.$ext"
+            [ -f $language_file ] && continue
+            echo "cp $file $dir/$name.$language.$ext"
+            cp "$file" "$dir/$name.$language.$ext"
+        done
+    done
+
+    bundle exec jekyll build --trace --verbose --destination _site_temp --config "_config.yml,_build/config.$language.yml"
+
+    for build_src in ./_site_temp/**/*; do
+        [ -f "$build_src" ] || continue
+        [[ " ${exclude_build_file[@]} " =~ " $build_src " ]] && continue
+        for dir in "${exclude_build_folder[@]}"; do
+            [[ "$build_src" == "$dir"* ]] && continue 2
+        done
+
+        build_dst="./_site/${build_src#./_site_temp/}"
+        echo "cp $build_src $build_dst"
+        cp "$build_src" "$build_dst"
+    done
+done
